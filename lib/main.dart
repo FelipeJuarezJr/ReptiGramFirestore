7162d3bbf96cfc03bf189c6f73ca6724525f2d97
FelipeJuarezJr <mr.felipe.juarez.jr@gmail.com>
2025-04-21 21:40:51 -0700

    ReptiGram icon and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(ReptiGram());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ReptiGram extends StatelessWidget {
+  const ReptiGram({super.key});
 
   // This widget is the root of your application.
   @override
